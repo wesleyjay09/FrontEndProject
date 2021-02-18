1e489f3535bf766f968a7b66dc357496e6acf35d
Wesley Coleman <77476122+wesleyjay09@users.noreply.github.com>
2021-02-18 15:36:33 -0600

    Refactored JS

--- app.js
+++ app.js
@@ -21,16 +21,26 @@ $(submit).attr('class', 'submit');
 $(submit).text('Search')
 $(searchBox).append(submit)
 
-var searchIcon = $('<i></i>');
-$(searchIcon).attr('class', 'fa fa-search');
-$(submit).append(searchIcon)
-
-
-
-
 var resultBox = $("<div></div>");
 $(resultBox).attr("id","results");
 $("body").append(resultBox);
+
+var loginContainer = $('<div></div>')
+$(loginContainer).attr('class', 'login')
+$("body").append(loginContainer);
+
+var userName = $('<input></input>');
+$(userName).attr('id', 'userName');
+$(userName).attr('placeholder','Username')
+$(userName).attr('type','text')
+$(loginContainer).append(userName);
+
+
+var button = $('<button></button>');
+$(button).attr('id','button');
+$(button).attr('class', 'submit');
+$(button).text('Login')
+$(loginContainer).append(button)
 
 
 $("#submit").click(function() {
@@ -39,47 +49,30 @@ $("#submit").click(function() {
     $.get(`http://api.weatherstack.com/current?access_key=fd35e38508293aefdabe73f4bb33a7a2&query=${userInput}`, (data) => {
         $("#results").empty()
         var data = (data)
-        
-        var city = data.location.name;
-        var temp = data.current.temperature;
-        var humidity = data.current.humidity;
-        var feelsLike = data.current.feelslike;
-        var weatherDes = data.current.weather_descriptions;
-        var windSpeed = data.current.wind_speed;
-        var windDirection = data.current.wind_dir;
 
-            var resultCard = $(`<span class="table"></span>`)
-            var cardLocation = $(`<h3 >City: ${city}<br></h3>`)
-            var cardWeather = $(`<h3 >Current: ${temp}<br></h3>`)
-            var cardFeels = $(`<h3 >Feels Like: ${feelsLike}<br></h3>`)
-            var cardHumidity =$(`<h3 >Humidity: ${humidity}<br></h3>`)
-            var cardWindSpeed =$(`<h3 >Wind Speed: ${windSpeed}<br></h3>`)
-            var cardWindDir = $(`<h3 >Wind Direction: ${windDirection}<br></h3>`)
-            var cardDescription = $(`<h3>${weatherDes}<br></h3>`)
+        var weather = {
+        city: data.location.name,
+        temp: data.current.temperature,
+        humidity: data.current.humidity,
+        feelsLike: data.current.feelslike,
+        weatherDes: data.current.weather_descriptions,
+        windSpeed: data.current.wind_speed,
+        windDirection: data.current.wind_dir
+        }
 
-            resultCard.append(cardLocation).append(cardWeather).append(cardFeels).append(cardHumidity).append(cardWindSpeed).append(cardWindDir).append(cardDescription)
-            resultBox.append(resultCard)
-        //  })
-      })  
- });
+        var resultCard = $(`<span class="table"></span>`)
+        var cardLocation = $(`<h3 >City: ${weather.city}<br></h3>`)
+        var cardWeather = $(`<h3 >Current: ${weather.temp}<br></h3>`)
+        var cardFeels = $(`<h3 >Feels Like: ${weather.feeslLike}<br></h3>`)
+        var cardHumidity =$(`<h3 >Humidity: ${weather.humidity}<br></h3>`)
+        var cardWindSpeed =$(`<h3 >Wind Speed: ${weather.windSpeed}<br></h3>`)
+        var cardWindDir = $(`<h3 >Wind Direction: ${weather.windDirection}<br></h3>`)
+        var cardDescription = $(`<h3>${weather.weatherDes}<br></h3>`)
 
-
-  var loginContainer = $('<div></div>')
-  $(loginContainer).attr('class', 'login')
-  $("body").append(loginContainer);
-
-  var userName = $('<input></input>');
-  $(userName).attr('id', 'userName');
-  $(userName).attr('placeholder','Username')
-  $(userName).attr('type','text')
-  $(loginContainer).append(userName);
-
-
-  var button = $('<button></button>');
-  $(button).attr('id','button');
-  $(button).attr('class', 'submit');
-  $(button).text('Login')
-  $(loginContainer).append(button)
+        resultCard.append(cardLocation).append(cardWeather).append(cardFeels).append(cardHumidity).append(cardWindSpeed).append(cardWindDir).append(cardDescription)
+        resultBox.append(resultCard)
+    })  
+});
 
  const saveToLocalStorage = () => {
     let inputName = userName.val()
@@ -91,14 +84,4 @@ $("#submit").click(function() {
 
   console.log(JSON.parse(localStorage.getItem('textInput')))
 
-
-     
-//  })
- 
-//  //add event listener that saves text input to local storage
-//  //when the user clicks login in, it will save the user name.
-//  //if there is a username input then save location searched last
-
-//  //save searched location of user
-
-//  //when user inputs username the last location searched will appear
+  
